1c7282c5739daec010454e5b7d546f901b1f15bf
brendalauriane <atblauriane@gmail.com>
2025-03-07 00:17:09 +0100

    creons le fichier Clock

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Alice"/>
+      <Welcome name="Bob"/> 
+      <Welcome name="Charlie"/>   
+      </div>
+  );
+}
+
+export default App;
+
